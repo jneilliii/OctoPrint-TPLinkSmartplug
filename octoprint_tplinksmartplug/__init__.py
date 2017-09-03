7a689deed94ba44e98c989359a7a9a72faa9c411
jneilliii <jneilliii+github@gmail.com>
2017-09-03 05:43:02 -0400

    test

--- octoprint_tplinksmartplug/__init__.py
+++ octoprint_tplinksmartplug/__init__.py
@@ -54,8 +54,13 @@ class tplinksmartplugPlugin(octoprint.plugin.SettingsPlugin,
 		
 	def check_status(self):
 		self._logger.info("Checking status.")
-		self._logger.info(self.sendCommand("info")["system"]["get_sysinfo"]["relay_state"])
-		self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="unknown"))
+		chk = self.sendCommand("info")["system"]["get_sysinfo"]["relay_state"]
+		if chk == 1:
+			self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="on"))
+		elif chk == 0:
+			self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="off"))
+		else:
+			self._plugin_manager.send_plugin_message(self._identifier, dict(currentState="unknown"))
 	
 	def get_api_commands(self):
 		return dict(turnOn=[],turnOff=[],checkStatus=[])
