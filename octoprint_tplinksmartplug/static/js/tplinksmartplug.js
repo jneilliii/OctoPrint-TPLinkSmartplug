fc5c23200547b292343208fe35eb172b2094c18d
jneilliii <jneilliii+github@gmail.com>
2017-09-24 04:11:08 -0400

    test

--- octoprint_tplinksmartplug/static/js/tplinksmartplug.js
+++ octoprint_tplinksmartplug/static/js/tplinksmartplug.js
@@ -44,6 +44,8 @@ $(function() {
 		
 		self.onAfterBinding = function() {			
 			self.poweroff_dialog = $("#tplinksmartplug_poweroff_confirmation_dialog");
+			self.arrSmartplugs(self.settings.settings.plugins.tplinksmartplug.arrSmartplugs());
+			self.checkStatuses();
 		}
 
         self.onEventSettingsUpdated = function (payload) {
