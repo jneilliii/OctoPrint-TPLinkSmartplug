b6bc3a6643cf84d00ccf6fe56d854646a84665e8
jneilliii <jneilliii+github@gmail.com>
2017-10-07 14:48:12 -0400

    test

--- octoprint_tplinksmartplug/static/js/tplinksmartplug.js
+++ octoprint_tplinksmartplug/static/js/tplinksmartplug.js
@@ -53,17 +53,13 @@ $(function() {
                 return;
             }
 			
-			console.log(data);
-			
 			plug = ko.utils.arrayFirst(self.settings.settings.plugins.tplinksmartplug.arrSmartplugs(),function(item){
 				return item.ip() === data.ip;
 				}) || {'ip':data.ip,'currentState':'unknown','btnColor':'#808080'};
-				
-			console.log(plug);
 			
 			if (data.gcodeon && plug.gcodeEnabled()) {
-				console.log(plug);
 				self.turnOn(plug);
+				return false;
 			}
 			
 			if (plug.currentState != data.currentState) {
