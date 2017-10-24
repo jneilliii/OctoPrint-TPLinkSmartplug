06279d3955581ebcd589c4e7126fb37b14a265f4
jneilliii <jneilliii+github@gmail.com>
2017-10-24 14:22:19 -0400

    Update setup.py
    Increment version.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_tplinksmartplug"
 plugin_name = "OctoPrint-TPLinkSmartplug"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.7.1"
+plugin_version = "0.7.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
