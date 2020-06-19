0d3b76d32acc20bf8091f364a418b223ba52abc4
jneilliii <jneilliii+github@gmail.com>
2020-06-19 17:33:54 -0400

    fix add plug issue #184

--- octoprint_tplinksmartplug/static/js/tplinksmartplug.js
+++ octoprint_tplinksmartplug/static/js/tplinksmartplug.js
@@ -272,7 +272,8 @@ $(function() {
 								'thermal_runaway':ko.observable(false),
 								'event_on_error':ko.observable(false),
 								'event_on_disconnect':ko.observable(false),
-								'automaticShutdownEnabled':ko.observable(false)});
+								'automaticShutdownEnabled':ko.observable(false),
+								'event_on_upload ':ko.observable(false)});
 			self.settings.settings.plugins.tplinksmartplug.arrSmartplugs.push(self.selectedPlug());
 			$("#TPLinkPlugEditor").modal("show");
 		}
